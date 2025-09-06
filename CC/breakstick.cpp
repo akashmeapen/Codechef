b22979e9fb680b98557d9262af57b99d7b530b11
Akash Modayil Eapen <akash.modayil@gmail.com>
2025-09-06 08:56:00 +0530

    Create breakstick.cpp

---
+++ CC/breakstick.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        if(x%2==0 && n%2!=0)
+            cout<<"NO"<<endl;
+        else
+            cout<<"YES"<<endl;
+    }
+}
