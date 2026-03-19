4a4de4da11e18b1df598ddf6c111b614ea77636f
akashmeapen <akash.modayil@gmail.com>
2026-03-19 21:06:06 +0530

    maxsum

--- CC/maxsum.cpp
+++ CC/maxsum.cpp
@@ -2,8 +2,33 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    
+    vector<long long> v(n), d(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    for (int i = 0; i < n; i++) cin >> d[i];
+    
+    priority_queue<pair<long long, long long>> pq;
+    
+    for (int i = 0; i < n; i++) {
+        pq.push(make_pair(v[i], d[i]));
+    }
+    
+    long long total = 0;
+    
+    for (int i = 0; i < m; i++) {
+        long long val = pq.top().first;
+        long long dec = pq.top().second;
+        pq.pop();
+        
+        if (val <= 0) break;
+        
+        total += val;
+        pq.push(make_pair(val - dec, dec));
+    }
+    
+    cout << total << endl;
     
     return 0;
 }
